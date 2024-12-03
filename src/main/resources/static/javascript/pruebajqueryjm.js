ce87b52fdcfdc2ad977c211388aaac0f7c1c3f05
Juanma <Juanma888@gmail.com>
2024-12-03 21:20:55 +0100

    Prueba jQuery 02.  Enredando.

--- src/main/resources/static/javascript/pruebajqueryjm.js
+++ src/main/resources/static/javascript/pruebajqueryjm.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
+    
     $("button").click(function(){
       $("label").hide();
+    });
+
+    $("#descripcion1").mouseenter(function(){
+      alert("Has entrado con el ratón en la caja de texto 'descripción'");
+    });
+
+    $("#estado1").hover(function(){
+      alert("Estás pasando por encima de 'estado1' con el ratón");
+    },
+    function(){
+      alert("Ahora ya no, que lo sepas.");
+    });
+
+    $("#texto1").focus(function(){
+      $(this).css("background-color", "red");
     });
   });
